75e9c8db51b9aac197a7f6f6e5e8f37afbe623bd
Gideon Clegg <19.gideon.clegg@ksd.org>
2018-12-14 10:29:06 -0800

    WHEEEEE

--- Assets/Scripts/PlayerPhysObj.cs
+++ Assets/Scripts/PlayerPhysObj.cs
@@ -20,33 +20,36 @@ public class PlayerPhysObj : PhysicsObject {
 
     protected override void ComputeVelocity()
     {
-        if (!isStopped)
+        if (Time.timeScale != 0)
         {
-            Vector2 move = Vector2.zero;
-            move.x = Input.GetAxis("Horizontal");
-            if(move.x > 0) { animator.SetBool("FaceLeft", false); }
-            else if(move.x < 0) { animator.SetBool("FaceLeft", true); }
-            if (Mathf.Abs(move.x) > 0.1f) { animator.SetBool("IsMoving", true); }
-            else { animator.SetBool("IsMoving", false); }
-            move.y = Input.GetAxis("Vertical");
-            if (Input.GetButtonDown("Jump") && grounded && canJump)
+            if (!isStopped)
             {
-                animator.SetTrigger("Jump");
-                velocity.y += jumpTakeOffSpeed;
-                canJump = false;
-                isJumping = true;
+                Vector2 move = Vector2.zero;
+                move.x = Input.GetAxis("Horizontal");
+                if (move.x > 0) { animator.SetBool("FaceLeft", false); }
+                else if (move.x < 0) { animator.SetBool("FaceLeft", true); }
+                if (Mathf.Abs(move.x) > 0.1f) { animator.SetBool("IsMoving", true); }
+                else { animator.SetBool("IsMoving", false); }
+                move.y = Input.GetAxis("Vertical");
+                if (Input.GetButtonDown("Jump") && grounded && canJump)
+                {
+                    animator.SetTrigger("Jump");
+                    velocity.y += jumpTakeOffSpeed;
+                    canJump = false;
+                    isJumping = true;
+                }
+                if (Input.GetButtonUp("Jump") && isJumping)
+                {
+                    velocity.y = velocity.y / 2;
+                }
+                if (Mathf.Sign(move.x) != Mathf.Sign(direction) && move.x != 0) { direction = -direction; }
+                targetVelocity.x = move.x * speed;
+                overrideVelocity.y = move.y * climbSpeed;
             }
-            if(Input.GetButtonUp("Jump") && isJumping)
+            else
             {
-                velocity.y = velocity.y / 2;
+                if (!gameObject.GetComponent<PlayerMain>().isAttack) { isStopped = false; }
             }
-            if(Mathf.Sign(move.x) != Mathf.Sign(direction) && move.x != 0) { direction = -direction; }
-            targetVelocity.x = move.x * speed;
-            overrideVelocity.y = move.y * climbSpeed;
-        }
-        else
-        {
-            if (!gameObject.GetComponent<PlayerMain>().isAttack) { isStopped = false; }
         }
     }
 }
